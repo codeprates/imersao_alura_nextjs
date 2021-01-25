e8ea21424ee36a9c857111a4841aac2cd6f8b61b
PratesLJ <prateslj@gmail.com>
2021-01-25 19:38:15 -0300

    Codigo ao final da primeira aula da imersao

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,55 @@
 import { createGlobalStyle, ThemeProvider } from 'styled-components'
+import db from '../db.json';
 
 const GlobalStyle = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
   body {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    /* New styles */
+    display: flex;
+    flex-direction: column;
+    font-family: 'Lato', sans-serif;
+    // Deixa branco no começo
+    color: ${({ theme }) => theme.colors.contrastText};
+  }
+  html, body {
+    min-height: 100vh;
+  }
+  #__next {
+    flex: 1;
+    display: flex;
+    flex-direction: column;    
   }
 `
 
-const theme = {
-  colors: {
-    primary: '#0070f3',
-  },
-}
+// Após importar o db.json, comentar o bloco abaixo
+// const theme = {
+//   colors: {
+//     primary: '#0070f3',
+//   },
+// }
+// e substituir por
+const theme = db.theme;
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <GlobalStyle />
       <ThemeProvider theme={theme}>
+        <GlobalStyle />
         <Component {...pageProps} />
       </ThemeProvider>
     </>
   )
 }
+// É preciso mover o GlobalStyles para dentro da tag ThemProvider para que o
+// global seja aplicado a todos os componentes que foram criados
+// de:
+{/* <GlobalStyle />
+<ThemeProvider theme={theme}> */}
+// para:
+{/* <ThemeProvider theme={theme}>
+  <GlobalStyle /> */}
